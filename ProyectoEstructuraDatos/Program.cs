05052d779cfd4f548b958eb4e1460d86dae739a9
JoeVillalobos <jodani0712@gmail.com>
2024-02-15 21:12:20 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoEstructuraDatos/Program.cs
@@ -0,0 +1,92 @@
+﻿class Program
+{
+    static string [] servicios = new string[10];
+    static int[] montos = new int[10];
+    static int[] codigosCaja = new int[10];
+    static int index = 0;
+
+    static void Main(string[] args)
+    {
+        int opcion;
+        do
+        {
+            Console.WriteLine("Menú Principal");
+            Console.WriteLine("1. Inicializar Vectores");
+            Console.WriteLine("2. Realizar Pagos");
+            Console.WriteLine("3. Consultar Pagos");
+            Console.WriteLine("4. Modificar Pagos");
+            Console.WriteLine("5. Eliminar Pagos");
+            Console.WriteLine("6. Submenú Reportes");
+            Console.WriteLine("7. Salir");
+            Console.Write("Seleccione una opción: ");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    InicializarVectores();
+                    break;
+                case 2:
+                    RealizarPagos();
+                    break;
+                case 3:
+                    ConsultarPagos();
+                    break;
+                case 4:
+                    ModificarPagos();
+                    break;
+                case 5:
+                    EliminarPagos();
+                    break;
+                case 6:
+                    SubmenuReportes();
+                    break;
+                case 7:
+                    Console.WriteLine("Saliendo del programa...");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Por favor, seleccione nuevamente.");
+                    break;
+            }
+
+        } while (opcion != 7);
+    }
+
+    static void SubmenuReportes()
+    {
+        int opcion;
+        do
+        {
+            Console.WriteLine("Submenú Reportes");
+            Console.WriteLine("1. Ver todos los Pagos");
+            Console.WriteLine("2. Ver Pagos por tipo de Servicio");
+            Console.WriteLine("3. Ver Pagos por código de caja");
+            Console.WriteLine("4. Ver Dinero Comisionado por servicios");
+            Console.WriteLine("5. Regresar Menú Principal");
+            Console.Write("Seleccione una opción: ");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    ConsultarPagos();
+                    break;
+                case 2:
+                    VerPagosPorTipoServicio();
+                    break;
+                case 3:
+                    VerPagosPorCodigoCaja();
+                    break;
+                case 4:
+                    VerDineroComisionado();
+                    break;
+                case 5:
+                    Console.WriteLine("Regresando al Menú Principal...");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Por favor, seleccione nuevamente.");
+                    break;
+            }
+
+        } while (opcion != 5);
+    }
